c0692ad81457ee21c3804d8685e2a47df8327705
Mark Anthony Saromines <tonysaromines@gmail.com>
2018-07-29 21:46:51 +0800

    modified tony.ph logo

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React, { Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import Tilt from 'react-tilt';
+
 import './App.css';
 import logo from './tony1.png';
 
@@ -36,8 +38,13 @@ class App extends Component {
 			<h2>Loading...</h2>
 		:
 			 
-			<div className="tc">		
-				<img src={logo} alt="tonyph" /> 
+			<div className="tc">
+				<div className="mt5">
+					<Tilt className="Tilt center ba ba b--dotted bw2 b--light-blue" options={{ max : 25 }} style={{ height: 203, width: 191 }} >	
+					 <img className="Tilt-inner" src={logo} alt="tonyph" /> 
+					</Tilt>	
+				</div>					
+
 				<h1 className="f1">RoboFriends</h1>					
 				<SearchBox searchChange= {this.onSearchChange}/>		
 				<Scroll>
